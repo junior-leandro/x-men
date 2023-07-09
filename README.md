f435680e479f6e911435dea03d56e8290cfa0145
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-07-09 18:17:28 -0300

    finish

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 
 
 Apesar de já ter um bom conhecimento em HTML, CSS e JavaScript, gostei muito do projeto e da didática dos irmãos do Dev em Dobro, e nunca é demais praticar! Então, bora adicionar esse projetinho maneiro ao gitHub e absorver mais conhecimentos em desenvolvimento frontend!
+
+<br/><br/><br/><br/>
+
+### ⚠ Importante
+
+#### Este projeto não foi feito por mim, apenas acompanhei as aulas do projeto disponibilizadas ao vivo no canal [Dev em Dobro](https://www.youtube.com/@DevemDobro) no YouTube e publiquei no meu gitHub.
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,3 @@
-/* 
-  O que precisamos fazer? - quando passar o mouse em cima do personagem na lista temos que adicionar a borda azul de seleção na imagem pequena do personagem e mostrar a imagem, o nome e o texto grande do personagem que está selecionado
-
-    OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
-        passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
-        
-        passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
-        
-        passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele 
-
-    OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
-        passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
-        passo 2 - alterar a imagem do personagem grande
-        passo 3 - alterar o nome do personagem grande
-        passo 4 - alterar a descrição do personagem grande
-*/
 
 // OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
 // passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
